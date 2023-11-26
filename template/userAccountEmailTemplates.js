5422c89e88945618ee849ebfb4ce982c50ccc4a0
OussemaBhenni <oussemabenhenni@gmail.com>
2023-11-26 22:24:51 +0100

    some edit

--- template/userAccountEmailTemplates.js
+++ template/userAccountEmailTemplates.js
@@ -1,108 +1,145 @@
-/**
- * Confirm user email to activate account to access to the app
- * @param {String} fullName User full name
- * @param {String} API_ENDPOINT Depend on the app running localy or server
- * @param {String} confirmationCode Generated unique code
- */
-const singUpConfirmationEmailTemplate = (
-  nom,
-    API_ENDPOINT,
-    email,
-    confirmationCode,
-  ) => `
+const singUpConfirmationEmailTemplate = (nom, API_ENDPOINT, email, confirmationCode) => `
   <!DOCTYPE html>
-  <html>
+  <html lang="en">
     <head>
-      <meta http-equiv="Content-Type" content="text/html;utf-8" />
-      <title>
-        Confirmation de votre inscription
-      </title>
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Confirmation de votre inscription</title>
+      <style>
+        body {
+          font-family: 'Helvetica Neue', Arial, sans-serif;
+          background-color: #f5f5f5;
+          margin: 0;
+          padding: 20px;
+        }
+        .container {
+          max-width: 600px;
+          margin: 0 auto;
+          background-color: #ffffff;
+          padding: 20px;
+          border-radius: 8px;
+          box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+        h1 {
+          color: #007BFF;
+        }
+        p {
+          color: #333;
+          line-height: 1.6;
+        }
+        a {
+          color: #007BFF;
+          text-decoration: none;
+          font-weight: bold;
+        }
+      </style>
     </head>
-  
-    <body style="max-width: 400px">
-      <div>
-        <p>Bonjour ${nom},</p>
-        <p>
-          Nous vous remercions pour votre inscription.
-        </p>
+    <body>
+      <div class="container">
+        <h1>Bonjour ${nom},</h1>
+        <p>Merci pour votre inscription.</p>
         <p>Votre nom d'utilisateur est : ${email}</p>
         <p>Veuillez suivre ce lien pour activer votre compte :</p>
-        <a href="${API_ENDPOINT}/account/${confirmationCode}/enable"
-          >Je confirme que je souhaite activer mon compte
-        </a>
+        <a href="${API_ENDPOINT}/account/${confirmationCode}/enable">Je confirme que je souhaite activer mon compte</a>
         <p>Cordialement.</p>
       </div>
     </body>
   </html>
-  
-  `;
-  
-  /**
-   * Send Email to reset password
-   * @param {String} nom User full name
-   * @param {*} email User email
-   * @param {String} API_ENDPOINT Depend on the app running localy or server
-   * @param {String} token Generated unique code
-   * @returns
-   */
-  const forgotPasswordEmailTemplate = (nom, email, API_ENDPOINT, token) => `
+`;
+
+const forgotPasswordEmailTemplate = (nom, email, API_ENDPOINT, token) => `
   <!DOCTYPE html>
-  <html>
+  <html lang="en">
     <head>
-      <title>
-        Réinitialisation de votre mot de passe
-      </title>
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Réinitialisation de votre mot de passe</title>
+      <style>
+        body {
+          font-family: 'Helvetica Neue', Arial, sans-serif;
+          background-color: #f5f5f5;
+          margin: 0;
+          padding: 20px;
+        }
+        .container {
+          max-width: 600px;
+          margin: 0 auto;
+          background-color: #ffffff;
+          padding: 20px;
+          border-radius: 8px;
+          box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+        h1 {
+          color: #007BFF;
+        }
+        p {
+          color: #333;
+          line-height: 1.6;
+        }
+        a {
+          color: #007BFF;
+          text-decoration: none;
+          font-weight: bold;
+        }
+      </style>
     </head>
     <body>
-      <div>
-        <p>Bonjour ${nom},</p>
-        <p>
-          Vous avez récemment fait une demande de réinitialisation du mot de passe
-          de votre compte : ${email}
-        </p>
-        <p>
-          Votre code de réinitialisation est :  ${token}
-        </p>
-        <p>
-          Utilisez ce code pour commencer le processus de réinitialisation.
-        </p>
+      <div class="container">
+        <h1>Bonjour ${nom},</h1>
+        <p>Vous avez récemment fait une demande de réinitialisation du mot de passe de votre compte : ${email}</p>
+        <p>Votre code de réinitialisation est : ${token}</p>
+        <p>Utilisez ce code pour commencer le processus de réinitialisation.</p>
         <p>Cordialement.</p>
       </div>
     </body>
   </html>
-  
-  `;
-  
-  /**
-   * Confirmation email after successful reset password
-   * @param {String} nom User full name
-   * @returns
-   */
-  const resetPasswordConfirmationEmailTemplate = (nom) => `
+`;
+
+const resetPasswordConfirmationEmailTemplate = (nom) => `
   <!DOCTYPE html>
-  <html>
+  <html lang="en">
     <head>
-      <title>
-        Confirmation de réinitialisation du mot de passe
-      </title>
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Confirmation de réinitialisation du mot de passe</title>
+      <style>
+        body {
+          font-family: 'Helvetica Neue', Arial, sans-serif;
+          background-color: #f5f5f5;
+          margin: 0;
+          padding: 20px;
+        }
+        .container {
+          max-width: 600px;
+          margin: 0 auto;
+          background-color: #ffffff;
+          padding: 20px;
+          border-radius: 8px;
+          box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+        h1 {
+          color: #007BFF;
+        }
+        p {
+          color: #333;
+          line-height: 1.6;
+        }
+      </style>
     </head>
     <body>
-      <div>
-      <p>Bonjour ${nom},</p>
+      <div class="container">
+        <h1>Bonjour ${nom},</h1>
         <p>Votre mot de passe a été réinitialisé avec succès.</p>
-        <p>
-          Vous pouvez maintenant vous connecter avec votre nouveau mot de passe.
-        </p>
+        <p>Vous pouvez maintenant vous connecter avec votre nouveau mot de passe.</p>
         <p>Cordialement.</p>
       </div>
     </body>
   </html>
-  `;
-  
-  // export module
-  module.exports = {
-    singUpConfirmationEmailTemplate,
-    forgotPasswordEmailTemplate,
-    resetPasswordConfirmationEmailTemplate,
-  };
-  
+`;
+
+// export module
+module.exports = {
+  singUpConfirmationEmailTemplate,
+  forgotPasswordEmailTemplate,
+  resetPasswordConfirmationEmailTemplate,
+};
